3a9275fec6536e738d5fa4220f53c4a1b06b7f78
aman <amanmeraj.1234@gmail.com>
2025-11-24 14:33:43 +0530

    Shubhechha Delivery First Commit

---
+++ app/src/main/java/com/subh/shubhechhadelivery/Activities/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.subh.shubhechhadelivery.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.subh.shubhechhadelivery.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
